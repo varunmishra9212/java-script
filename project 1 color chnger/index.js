b457543cb45078b96b23b350820f3912bc9940aa
varunmishra9212 <varunmishra9212@gmail.com>
2025-03-01 23:11:17 +0530

    project 1 complete

--- project 1 color chnger/index.js
+++ project 1 color chnger/index.js
@@ -1,5 +1,20 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelectorAll("body")
-buttons.forEach(function (button){
-  console.log(button)
-})
+const button = document.querySelectorAll('.button');
+ const body = document.querySelector('body');
+
+button.forEach(function (button){
+  console.log(button);
+  button.addEventListener('click' , function(e){
+    
+    console.log(e.target)
+  if(e.target.id ==='grey') {
+  body.style.backgroundColor = e. target.id;}
+  if(e.target.id ==='white') {
+    body.style.backgroundColor = e. target.id;}
+    if(e.target.id ==='yellow') {
+      body.style.backgroundColor = e. target.id;}
+      if(e.target.id ==='blue') {
+        body.style.backgroundColor = e. target.id;}
+        if(e.target.id ==='green') {
+          body.style.backgroundColor = e. target.id;}
+  });
+});
